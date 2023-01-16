a2f9996e644ce44909802f22abdb217f87efd602
Louis-Jean Teitelbaum <ljt@meidosem.com>
2023-01-16 15:22:15 +0100

    fix: Comment out unused controller actions

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -10,49 +10,50 @@ class ConversationsController < ApplicationController
   def show
   end
 
-  # GET /conversations/new
-  def new
-    @conversation = Conversation.new
-  end
-
-  # GET /conversations/1/edit
-  def edit
-  end
-
-  # POST /conversations
-  def create
-    @conversation = Conversation.new(conversation_params)
-
-    if @conversation.save
-      redirect_to @conversation, notice: "Conversation was successfully created."
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /conversations/1
-  def update
-    if @conversation.update(conversation_params)
-      redirect_to @conversation, notice: "Conversation was successfully updated."
-    else
-      render :edit, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /conversations/1
-  def destroy
-    @conversation.destroy
-    redirect_to conversations_url, notice: "Conversation was successfully destroyed."
-  end
+  #   # GET /conversations/new
+  #   def new
+  #     @conversation = Conversation.new
+  #   end
+  #
+  #   # GET /conversations/1/edit
+  #   def edit
+  #   end
+  #
+  #   # POST /conversations
+  #   def create
+  #     @conversation = Conversation.new(conversation_params)
+  #
+  #     if @conversation.save
+  #       redirect_to @conversation, notice: "Conversation was successfully created."
+  #     else
+  #       render :new, status: :unprocessable_entity
+  #     end
+  #   end
+  #
+  #   # PATCH/PUT /conversations/1
+  #   def update
+  #     if @conversation.update(conversation_params)
+  #       redirect_to @conversation, notice: "Conversation was successfully updated."
+  #     else
+  #       render :edit, status: :unprocessable_entity
+  #     end
+  #   end
+  #
+  #   # DELETE /conversations/1
+  #   def destroy
+  #     @conversation.destroy
+  #     redirect_to conversations_url, notice: "Conversation was successfully destroyed."
+  #   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_conversation
-      @conversation = Conversation.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def conversation_params
-      params.fetch(:conversation, {})
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_conversation
+    @conversation = Conversation.find(params[:id])
+  end
+
+  # # Only allow a list of trusted parameters through.
+  # def conversation_params
+  #   params.fetch(:conversation, {})
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "conversations#index"
 
   resources :users
-  resources :conversations
+  resources :conversations, only: [:index, :show]
   resources :messages, only: [:new, :create]
   resources :inbound_messages, only: [:create]
   resources :outbound_messages, only: [:create]
