a5c89712bff2445cecb3a43d2438970f22b50ba5
Louis-Jean Teitelbaum <ljt@meidosem.com>
2023-01-16 15:58:57 +0100

    feat: default locale is now fr

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,8 @@ module CourtMessage
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    config.i18n.default_locale = :fr
+
     config.eager_load_paths << Rails.root.join("app", "providers")
   end
 end
