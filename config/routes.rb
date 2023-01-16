54aede392cbf6d86d145de5482d92baf72337f18
Louis-Jean Teitelbaum <ljt@meidosem.com>
2023-01-16 18:54:27 +0100

    feat: Initial Contact pane

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "conversations#index"
 
   resources :users
+  resources :contacts
   resources :conversations, only: [:index, :show]
   resources :messages, only: [:new, :create]
   resources :inbound_messages, only: [:create]
---
+++ test/controllers/contacts_controller_test.rb
@@ -0,0 +1,49 @@
+require "test_helper"
+
+class ContactsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @contact = create(:contact)
+    @temp = build(:contact)
+  end
+
+  test "should get index" do
+    get contacts_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_contact_url
+    assert_response :success
+  end
+
+  test "should create contact" do
+    assert_difference("Contact.count") do
+      post contacts_url, params: { contact: { name: @temp.name, email: @temp.email, phone: @temp.phone } }
+    end
+
+    assert_redirected_to contact_url(Contact.last)
+  end
+
+  test "should show contact" do
+    get contact_url(@contact)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_contact_url(@contact)
+    assert_response :success
+  end
+
+  test "should update contact" do
+    patch contact_url(@contact), params: { contact: { name: "Other name" } }
+    assert_redirected_to contact_url(@contact)
+  end
+
+  test "should destroy contact" do
+    assert_difference("Contact.count", -1) do
+      delete contact_url(@contact)
+    end
+
+    assert_redirected_to contacts_url
+  end
+end
