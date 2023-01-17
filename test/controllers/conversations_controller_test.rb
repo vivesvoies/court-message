d86435fad60ad03ba9361ed1b23182e4940c91dc
Louis-Jean Teitelbaum <ljt@meidosem.com>
2023-01-17 19:00:13 +0100

    test: Initial ConversationController test

--- test/controllers/conversations_controller_test.rb
+++ test/controllers/conversations_controller_test.rb
@@ -1,48 +1,19 @@
 require "test_helper"
-__END__
 class ConversationsControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @conversation = conversations(:one)
-  end
-
   test "should get index" do
+    @conversation = create(:conversation)
+
     get conversations_url
     assert_response :success
-  end
-
-  test "should get new" do
-    get new_conversation_url
-    assert_response :success
-  end
-
-  test "should create conversation" do
-    assert_difference("Conversation.count") do
-      post conversations_url, params: { conversation: {  } }
-    end
-
-    assert_redirected_to conversation_url(Conversation.last)
+    assert_select ".Conversation"
+    assert_select ".Conversation__contact", text: @conversation.title
   end
 
   test "should show conversation" do
+    @conversation = create(:conversation)
+
     get conversation_url(@conversation)
     assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_conversation_url(@conversation)
-    assert_response :success
-  end
-
-  test "should update conversation" do
-    patch conversation_url(@conversation), params: { conversation: {  } }
-    assert_redirected_to conversation_url(@conversation)
-  end
-
-  test "should destroy conversation" do
-    assert_difference("Conversation.count", -1) do
-      delete conversation_url(@conversation)
-    end
-
-    assert_redirected_to conversations_url
+    assert_select ".ConversationDetail__title", text: @conversation.title
   end
 end
