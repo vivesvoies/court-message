75880ab014c0ab78afe659a547eba41124011a94
Louis-Jean Teitelbaum <ljt@meidosem.com>
2023-01-18 17:16:37 +0100

    ui: Fix visual bugs in :has() 2-pane responsive

--- test/system/conversations_test.rb
+++ test/system/conversations_test.rb
@@ -15,21 +15,30 @@ class ConversationsTest < ApplicationSystemTestCase
   test "opening a conversation" do
     visit conversations_url
     click_on @conversation.contact.name_or_phone, match: :first
-    assert_selector ".Message", text: @message.content
+    assert_selector ".Message__content", text: @message.content
   end
 
   test "being responsive" do
     resize_to_mobile
 
     visit conversations_url
+    body_width = page.evaluate_script('document.body.getBoundingClientRect().width')
+    list_width = page.evaluate_script('document.getElementById("conversations").getBoundingClientRect().width')
+    assert_equal(list_width, body_width)
+
     click_on @conversation.contact.name_or_phone, match: :first
-    assert_selector ".Conversation__contact", text: @conversation.contact.name_or_phone, visible: :hidden
-    assert_selector ".Message__content", text: @message.content, visible: true
+    list_width = page.evaluate_script('document.getElementById("conversations").getBoundingClientRect().width')
+    conv_width = page.evaluate_script('document.getElementById("conversation_detail").getBoundingClientRect().width')
+    assert_equal(list_width, 0)
+    assert_equal(conv_width, body_width)
 
     resize_to_desktop
-    sleep 0.5
-    assert_selector ".Conversation__contact", visible: true
-    assert_selector ".Message__content", text: @message.content, visible: true
+    body_width = page.evaluate_script('document.body.getBoundingClientRect().width')
+    list_width = page.evaluate_script('document.getElementById("conversations").getBoundingClientRect().width')
+    conv_width = page.evaluate_script('document.getElementById("conversation_detail").getBoundingClientRect().width')
+    assert(list_width > 0)
+    assert(conv_width > 0)
+    assert_equal(conv_width + list_width, body_width)
   end
 
   test "streaming a new message" do
@@ -37,7 +46,7 @@ class ConversationsTest < ApplicationSystemTestCase
 
     @message.content = "New content!"
     @message.save
-
+    sleep 0.25
     assert_selector ".Message__content", text: "New content!"
   end
 
