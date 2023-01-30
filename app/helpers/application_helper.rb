2e9924f01e72b6a83b9df5d87209964472a655f7
Louis-Jean Teitelbaum <ljt@meidosem.com>
2023-01-30 18:43:56 +0100

    feat: Helper to show more readable dates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def time_stamp_for time
+    case
+    when time.today?
+      time.strftime("%H:%M")
+    when time.past? && (Time.zone.now - time) < 7.days
+      I18n.l(time, format: "%A")
+    else
+      I18n.l(time.to_date, format: :default)
+    end
+  end
 end
---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,17 @@
+require "test_helper"
+
+class ApplicationHelperTest < ActiveSupport::TestCase
+  include ApplicationHelper
+
+  def test_time_stamp_for
+    today = Time.zone.now - 5.hours
+    this_week = Time.zone.now - 4.days
+    past = Time.zone.now - 8.days
+    future = Time.zone.now + 1.day
+
+    assert_match(/\d+:\d+/, time_stamp_for(today))
+    assert_match(/\w+/, time_stamp_for(this_week)) # Careful, brittle for some languages
+    assert_match(/\d+\/\d+\/\d+/, time_stamp_for(past))
+    assert_match(/\d+\/\d+\/\d+/, time_stamp_for(future))
+  end
+end
