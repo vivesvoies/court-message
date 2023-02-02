e322b1241791b9709fc3a12f0e7628db78337730
Louis-Jean Teitelbaum <ljt@meidosem.com>
2023-02-02 16:57:07 +0100

    perf: Eager load records from Conversations controller

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -3,7 +3,9 @@ class ConversationsController < ApplicationController
 
   # GET /conversations
   def index
-    @conversations = Conversation.all.order(updated_at: :desc)
+    # TODO: eager loading EVERY MESSAGE, this is overkill. Should create a last_message_id column.
+    # See https://github.com/louije/court-message/issues/47
+    @conversations = Conversation.all.includes(:contact, :messages).order(updated_at: :desc)
   end
 
   # GET /conversations/1
@@ -14,7 +16,7 @@ class ConversationsController < ApplicationController
 
   # Use callbacks to share common setup or constraints between actions.
   def set_conversation
-    @conversation = Conversation.includes(:messages, :contact, :agents).find(params[:id])
+    @conversation = Conversation.includes({ messages: :sender }, :contact, :agents).find(params[:id])
   end
 
   # # Only allow a list of trusted parameters through.
