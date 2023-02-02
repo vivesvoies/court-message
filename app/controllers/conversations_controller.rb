868fa172dead214cdf7cbe7d273ae598f80aeaaf
Louis-Jean Teitelbaum <ljt@meidosem.com>
2023-02-02 16:56:31 +0100

    fix: Remove unused controller actions

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -10,41 +10,6 @@ class ConversationsController < ApplicationController
   def show
   end
 
-  #   # GET /conversations/new
-  #   def new
-  #     @conversation = Conversation.new
-  #   end
-  #
-  #   # GET /conversations/1/edit
-  #   def edit
-  #   end
-  #
-  #   # POST /conversations
-  #   def create
-  #     @conversation = Conversation.new(conversation_params)
-  #
-  #     if @conversation.save
-  #       redirect_to @conversation, notice: "Conversation was successfully created."
-  #     else
-  #       render :new, status: :unprocessable_entity
-  #     end
-  #   end
-  #
-  #   # PATCH/PUT /conversations/1
-  #   def update
-  #     if @conversation.update(conversation_params)
-  #       redirect_to @conversation, notice: "Conversation was successfully updated."
-  #     else
-  #       render :edit, status: :unprocessable_entity
-  #     end
-  #   end
-  #
-  #   # DELETE /conversations/1
-  #   def destroy
-  #     @conversation.destroy
-  #     redirect_to conversations_url, notice: "Conversation was successfully destroyed."
-  #   end
-
   private
 
   # Use callbacks to share common setup or constraints between actions.
