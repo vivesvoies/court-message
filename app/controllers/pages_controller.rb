817bd9d1a019e0ca42f6acac53788b9ac5f3510d
Louis-Jean Teitelbaum <ljt@meidosem.com>
2024-01-02 15:53:07 +0100

    fix: ui page specific method in pagescon

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,16 @@
+# Display static pages
 class PagesController < ApplicationController
   skip_authorization_check
 
   def show
-    @icons = Dir.glob("app/assets/images/icons/*").map { |f| File.basename(f, File.extname(f)) }
-    flash.now[:notice] = "This is a notice with auto-remove"
+    prepare_ui_page if params[:page] == "ui"
     render params[:page]
+  end
+
+  private
+
+  def prepare_ui_page
+    @icons = Dir.glob("app/assets/images/icons/*").map { |f| File.basename(f, File.extname(f)) }
+    flash.now[:notice] = "This is a notice with auto-remove" # rubocop:disable Rails/I18nLocaleTexts
   end
 end
