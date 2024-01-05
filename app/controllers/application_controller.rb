cda77030a46b8d359fa3000e219e6960b506796c
Louis-Jean Teitelbaum <ljt@meidosem.com>
2024-01-05 15:35:25 +0100

    deploy: create staging environemnt

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,18 +3,20 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :set_current
   check_authorization unless: :devise_controller?
-  
+
   private
-  
+
   class Forbidden < StandardError; end
-  
-  rescue_from CanCan::AccessDenied do |_exception|
-    render plain: "403 / Not authorized", status: :forbidden
-  end unless Rails.env.development?
-  
+
+  unless Rails.env.development?
+    rescue_from CanCan::AccessDenied do |_exception|
+      render plain: "403 / Not authorized", status: :forbidden
+    end
+  end
+
   def set_current
     Current.user = current_user
-    Current.phone_number = Rails.env.production? ? "33644639777" : "33644630057"
+    Current.phone_number = Rails.env.development? || Rails.env.test ? "33644630057" : "33644639777"
 
     slug = params[:team_id] || (params[:controller] == "teams" && params[:id])
     Current.team = Team.find_by(slug:) if slug
