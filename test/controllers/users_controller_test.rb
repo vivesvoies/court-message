00a25a8c89b7f1ce26925e98d381a491215adfb7
Amelie LALLEMAND <amelielallemand@Amelies-MacBook-Pro.local>
2024-02-14 17:38:41 +0100

    Remove test for create method

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -8,20 +8,20 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     @other_user = create(:user, role: :user, teams: [ @team ])
     @team_admin = create(:user, role: :team_admin, teams: [ @team ])
     @site_admin = create(:user, role: :site_admin, teams: [ @team ])
-    sign_in @team_admin
-    sign_in @site_admin
   end
 
   test "normal users should be able to access index" do
     sign_in @user
     get team_users_url(@team)
     assert_response :success
+    sign_out @user
   end
 
   test "team admins should be able to access index" do
     sign_in @team_admin
     get team_users_url(@team)
     assert_response :success
+    sign_out @team_admin
   end
 
   test "site admins should be able to access index of all teams" do
@@ -30,49 +30,69 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     get team_users_url(@other_team, @site_admin)
     assert_response :success
+    sign_out @site_admin
   end
 
   test "users should not be able to access index of another team" do
     sign_in @user
     get team_users_url(@other_team, @user)
     assert_response :forbidden
+    sign_out @user
   end
 
   test "team admins should not be able to access index of another team" do
     sign_in @team_admin
     get team_users_url(@other_team, @team_admin)
     assert_response :forbidden
+    sign_out @team_admin
   end
 
   test "should be able edit user when self" do
     sign_in @user
     get edit_team_user_url(@team, @user)
     assert_response :success
+    sign_out @user
   end
 
   test "team admins should be able to edit any user" do
     sign_in @team_admin
     get edit_team_user_url(@team, @other_user)
     assert_response :success
+    sign_out @team_admin
   end
 
   test "site admins should be able to edit any user" do
     sign_in @site_admin
     get edit_team_user_url(@team, @other_user)
     assert_response :success
+    sign_out @site_admin
   end
 
   test "should not edit user when not self" do
     sign_in @user
     get edit_team_user_url(@team, @other_user)
     assert_response :forbidden
+    sign_out @user
   end
 
-  test "users should not be able to create user" do
-    sign_in @user
-    get new_team_user_url(@team)
-    assert_response :forbidden
-  end
+  # TODO: uncomment when implementing create in UsersController
+  # test "users should not be able to create user" do
+  #   sign_in @user
+  #   get new_team_user_url(@team)
+  #   assert_response :forbidden
+  # end
+
+  # test "team admins should be able to create user" do
+  #   sign_in @team_admin
+  #   get new_team_user_url(@team)
+  #   assert_response :success
+  # end
+
+  # test "site admins should be able to create user" do
+  #   sign_in @site_admin
+  #   get new_team_user_url(@team)
+  #   assert_response :success
+  # end
 
   # FIXME: Check role modification in User Class
   # test "should allow site admin to update role" do
@@ -84,18 +104,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   #   assert_equal "team_admin", other_user.reload.role
   # end
 
-  test "team admins should be able to create user" do
-    sign_in @team_admin
-    get new_team_user_url(@team)
-    assert_response :success
-  end
-
-  test "site admins should be able to create user" do
-    sign_in @site_admin
-    get new_team_user_url(@team)
-    assert_response :success
-  end
-
+  # TODO: uncomment when implementing UsersController
   # test "should not allow user to update via UserController" do
   #   sign_in @user
   #   patch user_url(user), params: { user: { name: "New name", email: "new@email.com", password: "password123", password_confirmation: "password123" } }
