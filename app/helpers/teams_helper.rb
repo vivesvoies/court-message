3ada72bb25301068870ae5097cee99b79dc4c36d
Louis-Jean Teitelbaum <ljt@meidosem.com>
2024-02-18 20:33:43 +0100

    feat: Menu contents

--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -1,2 +1,8 @@
 module TeamsHelper
+  def menu_for(label, icon:, destination:, frame: :conversation_detail)
+    active = current_page?(destination)
+    render partial: "menu_item", locals: { label:, icon:, active:, destination:, frame: }
+  end
 end
+
+
