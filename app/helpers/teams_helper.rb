ffbb61332a1df1530a3279775f4fbcb96cf1e369
Louis-Jean Teitelbaum <ljt@meidosem.com>
2024-03-22 18:38:06 +0100

    feat: Clicking on open menu titles returns to Conversations

--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -1,6 +1,14 @@
 module TeamsHelper
-  def menu_for(label, icon:, destination:, frame: :conversation_detail)
+  def menu_for(label, icon:, destination:, frame: :content)
     active = current_page?(destination)
     render partial: "menu_item", locals: { label:, icon:, active:, destination:, frame: }
+  end
+
+  def menu_link_destination(team)
+    if params[:controller] == "conversations"
+      menu_team_path(team)
+    else
+      team_conversations_path(team)
+    end
   end
 end
