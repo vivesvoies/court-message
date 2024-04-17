99ffc08e2973b032767939a7aacebbb6b2cdafde
Louis-Jean Teitelbaum <ljt@meidosem.com>
2024-04-17 21:58:35 +0200

    feat: Revamp of contact pages
    - headers
    - scrolling on mobile
    - aligned buttons on mobile
    - splitting css files

--- test/helpers/contacts_helper_test.rb
+++ test/helpers/contacts_helper_test.rb
@@ -1,20 +1,4 @@
 require "test_helper"
 
 class ContactsHelperTest < ActiveSupport::TestCase
-  include ContactsHelper
-  include ActionView::Helpers::TagHelper
-
-  def test_info_tag
-    c = create(:contact)
-    assert_equal(
-      info_tag(c, key: :name, missing: "missing"),
-      "<div class=\"Contact__name\">#{c.name}</div>"
-    )
-
-    c.name = "  "
-    assert_equal(
-      info_tag(c, key: :name, missing: "missing"),
-      "<div class=\"Contact__name Contact__name--missing\">missing</div>"
-    )
-  end
 end
