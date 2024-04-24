b25a01192e518a9cef93e6b00ef92456e1815376
Louis-Jean Teitelbaum <ljt@meidosem.com>
2024-04-24 20:16:00 +0200

    Update capybara.rb

--- test/support/capybara.rb
+++ test/support/capybara.rb
@@ -1,21 +1,14 @@
-Capybara.register_driver :selenium_chrome_in_container do |app|
-  Capybara::Selenium::Driver.new(
-    app,
-    browser: :remote,
-    url: "http://selenium-hub:4444/wd/hub",
-    capabilities: :chrome
-  )
-end
-
-Capybara.register_driver :headless_selenium_chrome_in_container do |app|
-  chrome_options = Selenium::WebDriver::Chrome::Options.new
-  chrome_options.add_argument("--headless")
-  chrome_options.add_argument("--disable-gpu")
-
-  Capybara::Selenium::Driver.new(
-    app,
-    browser: :remote,
-    url: "http://selenium-hub:4444/wd/hub",
-    capabilities: chrome_options
-  )
+if ENV["CI"]
+  Capybara.register_driver :headless_chrome do |app|
+    options = Selenium::WebDriver::Chrome::Options.new(
+      args: %w[headless no-sandbox disable-gpu disable-dev-shm-usage],
+      binary: '/usr/bin/google-chrome'
+    )
+  
+    Capybara::Selenium::Driver.new(
+      app,
+      browser: :chrome,
+      options:
+    )
+  end
 end
