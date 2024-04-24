e39f90db37db26703bc2bf835351627b92ae7e2c
Louis-Jean Teitelbaum <ljt@meidosem.com>
2024-04-24 19:54:18 +0200

    Update application_system_test_case.rb

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,21 +3,14 @@ require "test_helper"
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   include Devise::Test::IntegrationHelpers
 
-  if ENV["HEADLESS_CHROME"] != "true"
-    driven_by :selenium_chrome_in_container
-  else
-    driven_by :headless_selenium_chrome_in_container
-  end
-
-  Capybara.server_host = "web"
-  Capybara.always_include_port = true
+  driven_by :selenium, using: :chrome, screen_size: [1400, 1400] = true
 
   def resize_to_mobile
     resize_window_to(428, 926)
   end
 
   def resize_to_desktop
-    resize_window_to(1080, 1024)
+    resize_window_to(1400, 1400)
   end
 
   private
