642cdefe745eebe90ec3009e506757b3a7415db7
Amelie LALLEMAND <amelielallemand@Amelies-MacBook-Pro.local>
2024-09-16 16:06:15 +0200

    Deleting duplicate method

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -64,10 +64,6 @@ class Conversation < ApplicationRecord
   def set_last_message(message)
     self.last_message = message
     save!
-
-    if message.sender_type == "User"
-      agents << message.sender unless agents.include?(message.sender)
-    end
   end
 
   def broadcast_conversation_update
