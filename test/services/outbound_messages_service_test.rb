7a298fecab1a0aca4c4e8c77c800515fd3b67539
Amelie LALLEMAND <amelielallemand@Amelies-MacBook-Pro.local>
2024-10-14 15:11:53 +0200

    Simplify test

--- test/services/outbound_messages_service_test.rb
+++ test/services/outbound_messages_service_test.rb
@@ -18,10 +18,9 @@ class OutboundMessagesServiceTest < ActiveSupport::TestCase
   end
 
   def test_submits_message_to_service
-    expected_uuid = SecureRandom.uuid
     @provider.expect(
       :send,
-      OpenStruct.new(message_uuid: expected_uuid, http_response: Net::HTTPSuccess.new(1.0, "200", "OK")),
+      OpenStruct.new(message_uuid: :message_uuid, http_response: Net::HTTPSuccess.new(1.0, "200", "OK")),
       from: Current.phone_number, to: @contact.phone, content: "Lorem Ipsum"
     )
 
