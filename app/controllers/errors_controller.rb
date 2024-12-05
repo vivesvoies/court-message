08be5b8841f30a5aadee7e3117ee98c3ef358cfb
Amelie LALLEMAND <amelielallemand@Amelies-MacBook-Pro.local>
2024-12-05 13:33:03 +0100

    Remove duplicate code

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -3,34 +3,20 @@ class ErrorsController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :forbidden, :not_found, :unprocessable, :internal_server ]
 
   def forbidden
-    render_error_page(403)
+    render "errors/error", status: 403
   end
 
   def not_found
-    render_error_page(404)
+    render "errors/error", status: 404
   end
 
   def unprocessable
-    render_error_page(422)
+    render "errors/error", status: 422
   end
 
   def internal_server
-    render_error_page(500)
+    render "errors/error", status: 500
   rescue => e
     render plain: "500 - Erreur interne du serveur", status: 500
-  end
-
-  private
-
-  def render_error_page(status)
-    error_types = {
-      403 => :forbidden,
-      404 => :not_found,
-      422 => :unprocessable,
-      500 => :internal_server
-    }
-
-    @error_type = error_types[status]
-    render "errors/error", status: status
   end
 end
