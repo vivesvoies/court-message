a37817820601cac93540dd1949d355f713acbea4
Amelie LALLEMAND <amelielallemand@Amelies-MacBook-Pro.local>
2024-12-09 11:02:59 +0100

    Fix typo

--- test/controllers/errors_controller_test.rb
+++ test/controllers/errors_controller_test.rb
@@ -10,32 +10,32 @@ class ErrorsControllerTest < ActionDispatch::IntegrationTest
   test "should render forbidden page with status 403" do
     get "/403"
     assert_response :forbidden
-    assert_match /403 - Accès refusé/, response.body
+    assert_match /403 — Accès refusé/, response.body
   end
 
   test "should render not found page with status 404" do
     get "/404"
     assert_response :not_found
-    assert_match /404 - Page non trouvée/, response.body
+    assert_match /404 — Page non trouvée/, response.body
   end
 
   test "should render unprocessable page with status 422" do
     get "/422"
     assert_response :unprocessable_entity
-    assert_match /422 - Entité non traitable/, response.body
+    assert_match /422 — Entité non traitable/, response.body
   end
 
   test "should render internal server error page with status 500" do
     get "/500"
     assert_response :internal_server_error
-    assert_match /500 - Erreur interne du serveur/, response.body
+    assert_match /500 — Erreur interne du serveur/, response.body
   end
 
   test "should render not found page for unknown routes" do
     simulate_production_error_handling do
       get "/unknown_routes"
       assert_response :not_found
-      assert_match /404 - Page non trouvée/, response.body
+      assert_match /404 — Page non trouvée/, response.body
     end
   end
 
@@ -44,7 +44,7 @@ class ErrorsControllerTest < ActionDispatch::IntegrationTest
       sign_in @user
       get team_url(@other_team, @user)
       assert_response :forbidden
-      assert_match /403 - Accès refusé/, response.body
+      assert_match /403 — Accès refusé/, response.body
       sign_out @user
     end
   end
