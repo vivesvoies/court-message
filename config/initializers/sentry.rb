dd05d6c2060f6520f540ca06c3af2d8d02af0a1a
Louis-Jean Teitelbaum <ljt@meidosem.com>
2026-08-08 18:48:35 +0200

    fix: Restrict Sentry to production and staging environments (#441)
    * Restrict Sentry to production and staging environments
    
    * Remove verbose comment

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,8 @@
 Sentry.init do |config|
   config.dsn = "https://4fadcf943f15d1ca6827eb2510f467e0@o4507402586357760.ingest.de.sentry.io/4507402590093392"
+
+  config.enabled_environments = %w[production staging]
+
   config.breadcrumbs_logger = [ :active_support_logger, :http_logger ]
 
   config.traces_sample_rate = 0.05
