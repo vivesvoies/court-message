31df4d06813db118f166103d84fddd83bbe84a9f
Louis-Jean Teitelbaum <ljt@meidosem.com>
2026-08-09 15:07:57 +0200

    Make system tests run: commit data, deliver broadcasts, wait instead of sleep

--- test/system/messages_test.rb
+++ test/system/messages_test.rb
@@ -4,7 +4,7 @@ class MessagesTest < ApplicationSystemTestCase
   setup do
     @user = create(:user)
     @team = @user.teams.first
-    @conversation = create(:conversation)
+    @conversation = create(:conversation, contact: create(:contact, team: @team))
     10.times do
       create(:inbound_message, conversation: @conversation, sender: @conversation.contact)
       create(:outbound_message, conversation: @conversation, sender: @user)
@@ -16,22 +16,32 @@ class MessagesTest < ApplicationSystemTestCase
   end
 
   test "streaming a new message" do
-    @message = create(:inbound_message, conversation: @conversation)
+    message = create(:inbound_message, conversation: @conversation)
 
     visit team_conversation_url(@team, @conversation)
-    sleep 0.5
-    @message.update(content: "New content!")
-    sleep 0.5
+    # Waiting for the message to render also waits for the stream subscription:
+    # broadcasting before it is live loses the update with nothing to retry.
+    assert_selector ".Message__content", text: message.content
+
+    message.update!(content: "New content!")
+
     assert_selector ".Message__content", text: "New content!"
   end
 
   test "anchoring the message list" do
     visit team_conversation_url(@team, @conversation)
+    assert_selector "#messages .Message__content", minimum: 1
 
-    current_scroll = page.evaluate_script('document.getElementById("messages").scrollTop')
-    @message = create(:inbound_message, conversation: @conversation)
-    sleep 0.5
-    new_scroll = page.evaluate_script('document.getElementById("messages").scrollTop')
-    assert(new_scroll > current_scroll)
+    scroll_before = message_list_scroll_top
+    create(:inbound_message, conversation: @conversation, content: "Anchor me")
+    assert_selector ".Message__content", text: "Anchor me"
+
+    assert scroll_before < message_list_scroll_top
+  end
+
+  private
+
+  def message_list_scroll_top
+    page.evaluate_script('document.getElementById("messages").scrollTop')
   end
 end
